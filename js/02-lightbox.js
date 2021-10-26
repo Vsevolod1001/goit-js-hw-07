f0c6b437ad8b7715c697c432bad9431c3b18182a
Vsevolod1001 <sevant.101@gmail.com>
2021-10-26 22:59:28 +0300

    дз 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,14 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryElem = document.querySelector('.gallery');
+
+galleryElem.innerHTML = galleryItems
+  .map(
+    ({ original, preview, description }) =>
+      `<li class="gallery__item"><a href="${original}" class="gallery__link"><img src="${preview}" alt="${description}" class="gallery__image"></a></li>`,
+  )
+  .join('');
+
+
+  let lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
